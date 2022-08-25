55dc61f0fd7d9022fe78e076ac1cf329fbcd4446
MenukaIshan <OpenSource@1234>
2022-08-26 00:42:32 +0530

    Initial commit

---
+++ src/main/java/com/musala/dronetracker/DroneTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.musala.dronetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musala/dronetracker/DroneTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
